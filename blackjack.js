181d84f17e0de6077e105e4d4b11d9099cb125fb
Justin Lee <justinlee1533@gmail.com>
2016-04-24 16:36:45 -0500

    display hands to html properly

--- blackjack.js
+++ blackjack.js
@@ -8,6 +8,22 @@
 var gameDeck = new Deck();	//Deck object
 var gameDealer = new Dealer();  //Dealer object
 var gamePlayer = new Player(); //player object
+var gameGame = new game();
+
+function playGame()
+{
+  gameGame.play();
+}
+
+function hit()
+{
+  gamePlayer.setHit();
+}
+
+function stay()
+{
+  gamePlayer.setStands();
+}
 
 
 /*
@@ -40,6 +56,7 @@ function Player()
   */
   this.setHit = function()
   {
+    alert("hitting");
     this.hits = true;
   }
 
@@ -61,96 +78,113 @@ function Player()
 */
 function game() //maybe needs to take parameters of a deck, player, and dealer?, return true if player wins, false if dealer wins?
 {
-  alert("New Game buster douglas");
-  //initialize Deck
-  gameDeck.initialize();
-
-  //shuffle Deck
-  gameDeck.shuffle();
-
-  //deal two cards to the player
-  gamePlayer.hand.push(gameDeck.pop());
-  gamePlayer.hand.push(gameDeck.pop());
-
-  //deal two cards to the dealer
-  gameDealer.hand.push(gameDeck.pop());
-  gameDealer.hand.push(gameDeck.pop());
-
-  //check to see if either player has a blackjack
-  if((this.handVal(gamePlayer.hand))==21 && (this.handVal(gamePlayer.hand) ==21)) //TODO: maybe code would be prettier with a busts function
+  this.play = function()
   {
-    //game is a tie, both players have natual black jack
-    return("tie");
-  }else if ((this.handVal(gamePlayer.hand))==21)
-  {
-    //player wins natural blackjack
-    return(true);
-  }else if (this.handVal(gamePlayer.hand) ==21)
-  {
-    //dealer wins natural blackjack
-    return(false);
-  }
+    console.log("playing game");
+    //initialize Deck
+    gameDeck.initialize();
 
+    //shuffle Deck
+    gameDeck.shuffle();
 
-  //players turn
-  do
-  {
-    alert("Hit or Stay");
-    this.wait();  //wait until one of the buttons is pressed
+    //deal two cards to the player
+    var c1 = gameDeck.cardArr.pop();
+    console.log("Player's first card:"+c1.rank);
+    gamePlayer.hand.push(c1);
 
-    if(gamePlayer.hits == true)
+    var c2 = gameDeck.cardArr.pop();
+    console.log("Player's second card: "+c2.rank);
+    gamePlayer.hand.push(c2);
+
+    //deal two cards to the dealer
+    var c3 = gameDeck.cardArr.pop();
+    console.log("Dealers's first card:"+c3.rank);
+    gameDealer.hand.push(c3);
+
+    var c4 = gameDeck.cardArr.pop();
+    console.log("Dealers's Second card:"+c4.rank);
+    gameDealer.hand.push(c4);
+
+    this.printHands();
+
+    //check to see if either player has a blackjack
+    if((this.handVal(gamePlayer.hand))==21 && (this.handVal(gamePlayer.hand) ==21)) //TODO: maybe code would be prettier with a busts function
     {
-      this.gamePlayer.hits = false;
-      gamePlayer.hand.push(gameDeck.pop());
+      //game is a tie, both players have natual black jack
+      return("tie");
+    }else if ((this.handVal(gamePlayer.hand))==21)
+    {
+      //player wins natural blackjack
+      return(true);
+    }else if (this.handVal(gamePlayer.hand) ==21)
+    {
+      //dealer wins natural blackjack
+      return(false);
+    }
 
-      if((this.handVal(gamePlayer.hand))>21)  //player has busted
+
+    //players turn
+    do
+    {
+      alert("Hit or Stay");
+      this.wait();  //wait until one of the buttons is pressed
+
+      if(gamePlayer.hits == true)
+      {
+        this.gamePlayer.hits = false;
+        gamePlayer.hand.push(gameDeck.cardArr.pop());
+
+        if((this.handVal(gamePlayer.hand))>21)  //player has busted
+        {
+          return(false);
+        }
+      }
+    }while(((this.handVal(gamePlayer.hand))<22)&& (gamePlayer.stands == false)) //TODO: make a hits function
+
+
+    //if stand, allow the computer to go
+
+    if(this.handVal(gameDealer.hand)<17)//if dealers hand< 17 hit
+    {
+      gameDealer.hand.push(gameDeck.cardArr.pop());
+
+      if((this.handVal(gameDealer.hand))>21)  //Dealer has busted
       {
         return(false);
       }
-    }
-  }while(((this.handVal(gamePlayer.hand))<22)&& (gamePlayer.stands == false)) //TODO: make a hits function
-
-
-  //if stand, allow the computer to go
-
-  if(this.handVal(gameDealer.hand)<17)//if dealers hand< 17 hit
-  {
-    gameDealer.hand.push(gameDeck.pop());
-
-    if((this.handVal(gameDealer.hand))>21)  //Dealer has busted
+    }else
     {
-      return(false);
+        gameDealer.stands = true;
     }
-  }else
-  {
-      gameDealer.stands = true;
+
+      //if stay, compare the two hands
+
+      if((this.handVal(gamePlayer.hand))>(this.handVal(gameDealer.hand)))
+      {
+        return(true); //player wins
+      }else if ((this.handVal(gamePlayer.hand))<(this.handVal(gameDealer.hand)))
+      {
+        return(false);  //dealer wins
+      }else if((this.handVal(gamePlayer.hand))==(this.handVal(gameDealer.hand)))
+      {
+        alert("The game is a tie");
+      }
+
   }
-
-    //if stay, compare the two hands
-
-    if((this.handVal(gamePlayer.hand))>(this.handVal(gameDealer.hand)))
-    {
-      return(true); //player wins
-    }else((this.handVal(gamePlayer.hand))<(this.handVal(gameDealer.hand)))
-    {
-      return(false);  //dealer wins
-    }else if(((this.handVal(gamePlayer.hand))==(this.handVal(gameDealer.hand))))
-    {
-      alert("The game is a tie");
-    }
 
   /*
   	@Pre: the dealers or players hand array is passed in
   	@Post: The value of their hand is computed
   	@Return: The value of their hand
   */
-  this.handVal = function(h)
+  this.handVal = function(arr)
   {
     var sum = 0;
-
-    for(var i = 0; i< h.length; i++)
+    console.log("this hand has "+arr.length+" cards");
+    for(var i=0; i< arr.length; i++)
     {
-      sum += h[i].rank;
+      console.log("Value of card "+i+" is: "+arr[i].rank);
+      sum += arr[i].rank;
     }
 
     return(sum);
@@ -166,8 +200,52 @@ function game() //maybe needs to take parameters of a deck, player, and dealer?,
     if (!gamePlayer.hits && !gamePlayer.stands)
     {
       //TODO: get the state of the hit and stand button, write functions, maybe
-      setTimeout(wait,3000);
+      setTimeout(this.wait,3000);
     }
+  }
+
+  /*
+    @Pre: None
+    @Post: The two current hands are written to the HTML document
+    @Return: None
+  */
+  this.printHands = function()
+  {
+    console.log("printing hands");
+
+    var hiddenHand =[];
+    hiddenHand[0] = "X";
+    console.log("Dealers Hand");
+    console.log("Dealers Hand length: "+gameDealer.hand.length);
+
+    for(var i=1; i<gameDealer.hand.length; i++)
+    {
+      var tempCard = gameDealer.hand[i];
+      var rankNo = tempCard.rank;
+      console.log("value of the card: "+rankNo);
+
+      hiddenHand[i] = rankNo;
+    }
+    hiddenHand = hiddenHand.toString();
+
+    //print the dealers hand, all cards except the first
+    document.getElementById("p1").innerHTML = hiddenHand;
+
+
+    console.log("Players Hand");
+  console.log("Players Hand length: "+gamePlayer.hand.length);
+    //print the players hand, all cards
+    var playerHand =[];
+    for(var i=0; i<gamePlayer.hand.length; i++)
+    {
+      var tempCard = gamePlayer.hand[i];
+      var rankNo = tempCard.rank;
+      console.log("value of the card: "+rankNo);
+
+      playerHand[i] = rankNo;
+    }
+    document.getElementById("p2").innerHTML = playerHand.toString();
+
   }
 }
 
@@ -200,28 +278,29 @@ function Deck()
   this.initialize = function()
   {
     this.cardArr = [];
-    for(var i = 1; i<13; i++ )
+    var newCard;
+    for(var i = 1; i<=13; i++ )
     {
-      for(var j= 0; j<4; j++)
+      for(var j = 0; j<4; j++)
       {
         if(j == 0)
         {
-          var newCard = new Card(i, "spades");
+          newCard = new Card(i, "spades");
           this.cardArr.push(newCard);
 
         }else if(j == 1)
         {
-          var newCard = new Card(i, "hearts");
+          newCard = new Card(i, "hearts");
           this.cardArr.push(newCard);
 
         }else if(j == 2)
         {
-          var newCard = new Card(i, "diams");
+          newCard = new Card(i, "diams");
           this.cardArr.push(newCard);
 
-        }else if(j == 4)
+        }else if(j == 3)
         {
-          var newCard = new Card(i, "clubs");
+          newCard = new Card(i, "clubs");
           this.cardArr.push(newCard);
 
         }
@@ -237,14 +316,20 @@ function Deck()
   */
   this.shuffle = function()
   {
+    console.log("Size of the deck: "+this.cardArr.length);
     if (this.loaded == true)
     {
+      var index1;
+      var index2;
+      var temp;
       for(var i = 0; i<100; i++) //perform 100 random shuffles
       {
         var index1 = Math.floor(Math.random()*51);
         var index2 = Math.floor(Math.random()*51); //get two random indexes
 
-        var temp = this.cardArr[index1];
+        console.log("Swapping index: "+index1+" with index "+ index2 )
+        console.log( "This swaps the: "+this.cardArr[index1].suit +" of "+ this.cardArr[index1].rank +" with the "+this.cardArr[index2].suit + " of " +this.cardArr[index2].rank);
+        temp = this.cardArr[index1];
         this.cardArr[index1] = this.cardArr[index2];
         this.cardArr[index2] = temp;
       }
